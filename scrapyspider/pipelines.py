68dd2bcdeb6277d14909fe5dd14b4ecd573093c6
wusisu <i@wusisu.com>
2018-02-01 17:26:15 +0800

    introduce mongo

--- scrapyspider/pipelines.py
+++ scrapyspider/pipelines.py
@@ -1,11 +1,65 @@
+import hashlib
+import urllib
+
+import pymongo
+import scrapy
 
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+class MongoPipeline(object):
 
+    collection_name = 'scrapy_items'
 
-class ScrapyspiderPipeline(object):
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+        self.client = None
+        self.db = None
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert_one(dict(item))
+        return item
+
+
+class ScreenshotPipeline(object):
+    """Pipeline that uses Splash to render screenshot of
+    every Scrapy item."""
+
+    SPLASH_URL = "http://localhost:8050/render.png?url={}"
+
+    def process_item(self, item, spider):
+        encoded_item_url = urllib.parse.quote(item["url"])
+        screenshot_url = self.SPLASH_URL.format(encoded_item_url)
+        request = scrapy.Request(screenshot_url)
+        dfd = spider.crawler.engine.download(request, spider)
+        dfd.addBoth(self.return_item, item)
+        return dfd
+
+    def return_item(self, response, item):
+        if response.status != 200:
+            # Error happened, return item.
+            return item
+
+        # Save screenshot to file, filename will be hash of url.
+        url = item["url"]
+        url_hash = hashlib.md5(url.encode("utf8")).hexdigest()
+        filename = "{}.png".format(url_hash)
+        with open(filename, "wb") as f:
+            f.write(response.body)
+
+        # Store filename in item.
+        item["screenshot_filename"] = filename
         return item
